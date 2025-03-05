e8678114a63711347dccd90c9b754afba8b79b78
Jaim Zuber <jaim@sharpfivesoftware.com>
2025-03-04 19:13:20 -0600

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Jaim Zuber on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Jaim Zuber on 3/4/25.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
